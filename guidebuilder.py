3a069e6d6746935c2f78ee2167c80a8479f7cf95
nate <naherrmann@gmail.com>
2017-03-29 14:18:24 -0700

    Added pair sorting to GuideBuilder. Now initializes with a dict of settings.

--- guidebuilder.py
+++ guidebuilder.py
@@ -18,7 +18,7 @@ class GuideBuilder(object):
                }
 
 
-  def __init__(self, filepath=None):
+  def __init__(self, settings=DEFAULTS):
     
     self.logger = log.getLogger(__name__)
 
@@ -28,19 +28,35 @@ class GuideBuilder(object):
     self.exon_edges = None
     self.gene_size = None
 
+    self.settings = settings
+
+    # Copies unspecified fields to settings from DEFAULTS
+    for key, value in DEFAULTS.items():
+      if key not self.settings.keys():
+        self.settings[key] = value
+
+    if 'input_path' in self.settings.keys():
+      self.read()
+
+
+
+  def read(self, filepath=None):
+    """Reads and processes the ChopChop results file in filepath.
+    Uses self.settings['filepath'] if no filepath argument is given."""
+
     if filepath is not None:
-      self.read(filepath)
+      self.settings['input_path'] = filepath
+    elif 'input_path' not in self.settings.keys():
+      self.logger.warning('Cannot read file: specify a filepath')
+      return
 
-
-
-  def read(self, filepath, exon_edges=None):
 
     self.sequences = []
     self.guidepairs = []
     self.exon_edges = exon_edges
 
 
-    self.file = open(filepath, 'r')
+    self.file = open(self.settings['input_path'], 'r')
 
     for i, line in enumerate(self.file):
 
@@ -76,11 +92,33 @@ class GuideBuilder(object):
 
 
   def sort_sequences(self, sequencelist, keystr=None):
-    """Sorts the list of sequences by a given field in place. 
+    """Sorts the list of sequences in place by a given field. 
     Sequences are sorted by ascending genomic location by default (no keystr)"""
 
     if keystr is None or keystr == "gen_loc":
       sequencelist.sort(key=lambda seq: seq.gen_loc)
+    else:
+      self.logger.warning("Cannot sort pair list: invalid key string")
+
+
+
+  def sort_pairs(self, pairlist, keystr=None):
+    """Sorts the list of GuidePairs in place by a given field. 
+    Pairs are sorted by ascending genomic location of seq1 by default (no keystr)"""
+
+    if keystr is None or keystr == "gen_loc" or keystr == "genomic_location":
+      pairlist.sort(key=lambda pair: pair.seq1.gen_loc)
+    elif keystr == "gen_sep" or keystr == "genomic_separation":
+      pairlist.sort(key=lambda pair: pair.genomic_separation)
+    elif keystr == "del_cont" or keystr == "deletion_content":
+      pairlist.sort(key=lambda pair: pair.deletion_content)
+    elif keystr == "del_frac" or keystr == "deletion_fraction":
+      pairlist.sort(key=lambda pair: pair.deletion_fraction)
+    else:
+      self.logger.warning("Cannot sort pair list: invalid key string")
+
+
+
 
 
 
@@ -116,11 +154,12 @@ class GuideBuilder(object):
 
 
 
-  def _filter_offtargets(self, max_offtargets=DEFAULTS['max_offtargets']):
+  def _filter_offtargets(self, max_offtargets=None):
     """Removes sequences for which any offsite count exceeds the given 
     max value. Filters to sequences with no offtargets by default."""
 
-    print 'max offtargets: {}'.format(max_offtargets)
+    if max_offtargets is None:
+      max_offtargets = self.settings['max_offtargets']
 
     if len(max_offtargets) != 4:
       self.logger.warning("Cannot filter targets: specify all four max offsite values")
@@ -154,13 +193,13 @@ class GuideBuilder(object):
       # can be removed from the original during iteration
       for start in list(start_seqs):
         # Pops start sequences that are too far upstream
-        if abs(seq.cut_site - start.cut_site) > DEFAULTS['separation_limit']:
+        if abs(seq.cut_site - start.cut_site) > self.settings['separation_limit']:
           start_seqs.pop(0)
         
         else:
           if start.cut_site == seq.cut_site:
             continue
-          if DEFAULTS['gRNA2_start_G']:
+          if self.settings['gRNA2_start_G']:
             self.guidepairs.append(gp.GuidePair(start, seq))
           else:
             self.guidepairs += [gp.GuidePair(start, g_seq) 
@@ -169,7 +208,7 @@ class GuideBuilder(object):
         start_seq_queue += seq.find_G_starts()
 
 
-      if seq.gene_loc_frac > DEFAULT['latest_gRNA1']:
+      if seq.gene_loc_frac > self.settings['latest_gRNA1']:
         break
 
 
